079461248186bd7acdfa5d338b43394e2b8bb95a
supreme2580 <victoromorogbe69@gmail.com>
2023-05-21 14:11:33 +0100

    suprememods

--- components/Vision.tsx
+++ components/Vision.tsx
@@ -26,7 +26,6 @@ export function Vision() {
                         </div>
                     </div>
                 </div>
-                <div></div>
             </div>
         </div>
     )
