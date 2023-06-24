7d6575ea2ad5af3713f4d8391a4b66b552a8bfa4
supreme2580 <victoromorogbe69@gmail.com>
2023-06-24 18:01:30 +0100

    suprememods

--- components/Preview.tsx
+++ components/Preview.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Preview() {
     return(
         <div>
-            <Image src={"/preview.png"} width={1000} height={750} alt="preview" className="hidden sm:block" />
+            <Image src={"/preview.png"} width={1000} height={1000} alt="preview" className="hidden sm:block" />
             <div className="w-full h-[0.5px] bg-gradient-to-r from-[#1017251A] via-white to-[#1017251A] absolute inset-x-0" />
         </div>
     )
