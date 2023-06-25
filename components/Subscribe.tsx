6ab677742cc9f228c1aa0ca07051fdc7de48d3f2
supreme2580 <victoromorogbe69@gmail.com>
2023-06-25 02:54:22 +0100

    suprememods

--- components/Subscribe.tsx
+++ components/Subscribe.tsx
@@ -4,15 +4,11 @@ export default function Subscribe() {
     return (
         <div className="w-full h-full max-w-lg lg:max-w-2xl">
             <div className="flex justify-between w-full h-[375px]">
-                <div className="mt-20 w-fit h-fit">
-                    <Image src={"/laser.png"} width={6} height={6} alt="laser" />
-                </div>
+                <Image src={"/laser.png"} width={6} height={6} alt="laser" />
                 <div>
                     <Image src={"/trans-logo.png"} width={250} height={250} alt="laser" />
                 </div>
-                <div className="mt-20 w-fit h-fit">
-                    <Image src={"/laser.png"} width={6} height={6} alt="laser" />
-                </div>
+                <Image src={"/laser.png"} width={6} height={6} alt="laser" />
             </div>
             <div className="w-full -mt-32 space-y-2 text-center">
                 <h1 className="text-3xl font-extrabold text-center text-transparent sm:text-5xl bg-clip-text bg-gradient-to-br from-trans-gray via-white to-trans-gray">Access DAOs through innovative tools</h1>
