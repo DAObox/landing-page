0cd551c8efc60049ecd3c6490d1925c32e2049d5
supreme2580 <victoromorogbe69@gmail.com>
2023-06-25 23:07:27 +0100

    suprememods

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,14 +5,14 @@ export default function Footer() {
     return (
         <div className="flex flex-col items-center w-screen h-full bg-dark-gray">
             <div className="w-full px-5 pt-8 space-y-20 lg:space-y-36 md:px-14 xl:max-w-6xl">
-                <div className="flex justify-between w-full">
+                <div className="flex flex-col w-full space-y-6 sm:space-y-0 sm:flex-row sm:justify-between">
                     <div>
                         <div className="flex items-center space-x-1.5">
                             <Image src={"/favicon.ico"} width={30} height={30} alt="icon" />
                             <h1 className="text-2xl font-semibold text-white">DAOBox</h1>
                         </div>
                     </div>
-                    <div className="flex space-x-20">
+                    <div className="flex flex-col space-y-6 sm:space-y-0 sm:flex-row sm:space-x-20">
                         <div className="flex flex-col space-y-4">
                             <h6 className="text-lg text-white">Product</h6>
                             <Link href={"/"} className="text-lg text-trans-ash">Lens Voting Plugin</Link>
