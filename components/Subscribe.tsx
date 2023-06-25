627f8097c97ee954fd58dcc93ef6162ffeb2d1b8
supreme2580 <victoromorogbe69@gmail.com>
2023-06-25 02:45:41 +0100

    suprememods

--- components/Subscribe.tsx
+++ components/Subscribe.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 export default function Subscribe() {
     return (
         <div className="w-full h-full max-w-lg lg:max-w-2xl">
-            <div className="flex justify-between w-full h-full">
-                <div className="">
+            <div className="flex justify-between w-full h-[375px]">
+                <div className="mt-20">
                     <Image src={"/laser.png"} width={6} height={6} alt="laser" />
                 </div>
                 <div>
                     <Image src={"/trans-logo.png"} width={250} height={250} alt="laser" />
                 </div>
-                <div className="">
+                <div className="mt-20">
                     <Image src={"/laser.png"} width={6} height={6} alt="laser" />
                 </div>
             </div>
