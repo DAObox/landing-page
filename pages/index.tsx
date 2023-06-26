e734cc9a916c6ebf9517dd95aec080136b74b03a
okhaimie-dev <oktonystark8@gmail.com>
2023-06-26 12:32:21 -0500

    color fix WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,36 +10,38 @@ import Footer from "../components/Footer";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex justify-center relative w-full min-h-screen bg-gray bg-[url('/mobile-gradient.png')] md:bg-[url('/tablet-gradient.png')] 
-    xl:bg-[url('/tablet-gradient.png')] bg-no-repeat bg-center bg-cover h-full">
-      <Head>
-        <title>DAOBox</title>
-        <link
-          rel="icon"
-          href="/favicon.ico"
-        />
-        <meta
-          name="description"
-          content="DAOBox is a decentralized organization that empowers communities through building tools to enable wider access to blockchain governance technology."
-        />
-        <meta
-          name="twitter:site"
-          content="@daobox_app"
-        />
-      </Head>
-      <main className="flex flex-col items-center justify-center w-full h-full">
-        <Navbar />
-        <div className="flex flex-col items-center w-full h-full px-5 pt-8 space-y-20 lg:space-y-36 md:px-14 xl:max-w-6xl">
-          <Hero />
-          <Preview />
-          <DaoTools />
-          <Experience />
-          <Subscribe />
-          <Footer />
-        </div>
-      </main>
-    </div>
-  );
+		<div
+			className="flex justify-center relative w-full min-h-screen bg-gray bg-[url('/mobile-gradient.png')] md:bg-[url('/tablet-gradient.png')] 
+    xl:bg-[url('/tablet-gradient.png')] bg-no-repeat bg-center bg-cover h-full"
+		>
+			<Head>
+				<title>DAOBox</title>
+				<link
+					rel="icon"
+					href="/favicon.ico"
+				/>
+				<meta
+					name="description"
+					content="DAOBox is a decentralized organization that empowers communities through building tools to enable wider access to blockchain governance technology."
+				/>
+				<meta
+					name="twitter:site"
+					content="@daobox_app"
+				/>
+			</Head>
+			<main className="flex bg-[#090C11] flex-col items-center justify-center w-full h-full">
+				<Navbar />
+				<div className="flex flex-col items-center w-full h-full px-5 pt-8 space-y-20 lg:space-y-36 md:px-14 xl:max-w-6xl">
+					<Hero />
+					<Preview />
+					<DaoTools />
+					<Experience />
+					<Subscribe />
+					<Footer />
+				</div>
+			</main>
+		</div>
+	);
 };
 
 export default Home;
