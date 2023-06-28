8970443f9a27fb266dd8f4a4a4e6cded563a3975
supreme2580 <victoromorogbe69@gmail.com>
2023-06-28 12:33:50 +0100

    suprememods

--- components/DaoTools.tsx
+++ components/DaoTools.tsx
@@ -28,7 +28,7 @@ export default function DaoTools() {
 					>
 						<div className="h-full">
 							<div className="w-full h-full p-6 rounded-lg bg-trans-gray-2">
-								<h5 className="text-2xl font-semibold leading-8 text-black">
+								<h5 className="text-2xl font-semibold leading-8 text-trans-gray-2">
 									Aragon Plugin
 								</h5>
 								<div className="w-full h-full bg-[url('/thin-grid.png')] md:bg-[url('/large-grid.png)] bg-no-repeat bg-cover bg-center pt-10 space-y-2">
@@ -165,7 +165,7 @@ export default function DaoTools() {
                 bg-gradient-to-br from-[#1017251A] via-white to-[#1017251A] h-full"
 							>
 								<div className="w-full h-full p-6 rounded-lg bg-trans-gray-2">
-									<h5 className="text-2xl font-semibold leading-8 text-black">
+									<h5 className="text-2xl font-semibold leading-8 text-trans-gray-2">
 										Aragon Plugin
 									</h5>
 									<div className="w-full h-full bg-[url('/thin-grid.png')] md:bg-[url('/large-grid.png)] bg-cover bg-no-repeat bg-center pt-24 space-y-2">
@@ -200,7 +200,7 @@ export default function DaoTools() {
                 bg-gradient-to-br from-[#1017251A] via-white to-[#1017251A] lg:hidden"
 				>
 					<div className="w-full p-6 rounded-lg h-fit bg-trans-gray-2">
-						<h5 className="text-2xl font-semibold leading-8 text-black">
+						<h5 className="text-2xl font-semibold leading-8 text-trans-gray-2">
 							Aragon Plugin
 						</h5>
 						<div className="w-full h-full bg-[url('/thin-grid.png')] md:bg-[url('/large-grid.png)] bg-cover bg-no-repeat bg-center pt-24 space-y-2">
